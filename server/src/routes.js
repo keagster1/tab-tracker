603bf27ff931d73d66de30911d66655f2bd3d88c
Keagan <k_lidwell@live.com>
2019-05-29 19:53:19 -0500

    Basic Validation for register using policy

--- server/src/routes.js
+++ server/src/routes.js
@@ -2,5 +2,5 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 
 module.exports = (app) => {
-    app.post('/register', AuthenticationController.register)
+    app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
 }
