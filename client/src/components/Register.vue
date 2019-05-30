a7bd45f5878ec63fa25f738292d80d42d94263dc
Keagan <k_lidwell@live.com>
2019-05-29 20:05:24 -0500

    Post Vuetify *Phew*

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,42 +1,51 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="email" v-model="email">
-    <br>
-    <input type="password" name="password" id placeholder="password" v-model="password">
-    <br>
-    <div class="error" v-html="error"></div>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout row>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <h1>Register</h1>
+          <input type="email" name="email" placeholder="email" v-model="email">
+          <br>
+          <input type="password" name="password" id placeholder="password" v-model="password">
+          <br>
+          <div class="error" v-html="error"></div>
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import AuthenticationService from '@/services/AuthenticationService'
+import AuthenticationService from "@/services/AuthenticationService";
 export default {
-  data () {
+  data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       error: null
-    }
+    };
   },
   methods: {
-    async register () {
+    async register() {
       try {
         const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
-        })        
+        });
       } catch (error) {
-        this.error = error.response.data.error
+        this.error = error.response.data.error;
       }
     }
   }
-}
+};
 </script>
 
 <style>
 .error {
-  color:red;
+  color: red;
 }
 </style>
