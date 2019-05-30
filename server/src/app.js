e8c218bcdf70ce992e3d04f3ee4fed74929a6eee
Keagan <k_lidwell@live.com>
2019-05-29 19:43:34 -0500

    Refactor and Beginnings of Authentication

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
-// Setup
-const app = express();
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(cors());
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
 
-// Basic Get
-app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email}. Your user was registered! Have fun!`
-    });
-});
+require('./routes')(app)
 
-// Make App Accessible. 
-app.listen(process.env.PORT || 8081);
+sequelize.sync()
+    .then(() => {
+
+        app.listen(config.port)
+        console.log(`Server started on port ${config.port}`)
+    })
